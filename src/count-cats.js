a00703f7cb0c11cf1e006c4e428aed705c7c2850
Alitalia60 <lita.study@bk.ru>
2021-09-15 23:08:19 +0300

    do test count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,7 +14,13 @@ import { NotImplementedError } from '../extensions/index.js';
  * ]) => 3`
  *
  */
-export default function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function countCats(matrix) {
+  let sum=0;
+  matrix.forEach(function (RowOfYard) {
+    RowOfYard.forEach(function (cell) {
+      if (cell == '^^') sum++;
+    });
+  });
+
+  return sum;
 }
