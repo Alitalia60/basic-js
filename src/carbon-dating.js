6eaa7365e43900c3f6bdf6a84233c23385ec21fc
Alitalia60 <lita.study@bk.ru>
2021-09-18 19:23:15 +0300

    feat: append dream-team solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -19,11 +19,22 @@ const HALF_LIFE_PERIOD = 5730;
  */
 export default function dateSample(sampleActivity) {
   let k = 0.693 / HALF_LIFE_PERIOD;
-  if (typeof sampleActivity != "string") return false;
-  if (Number(sampleActivity) == NaN) return false;
+  const CORRECT_SYMBOLS = "0123456789.";
 
   if (sampleActivity == undefined) return false;
-  if (sampleActivity > MODERN_ACTIVITY) return false;
+  if (typeof sampleActivity != "string") return false; // not a string
+  //if (Number(sampleActivity) == NaN) return false;
+  let flag = true;
+  for (let index = 0; index < sampleActivity.length; index++) {
+    if (CORRECT_SYMBOLS.indexOf(sampleActivity[index], 0) == -1) {
+      flag = false;
+      break;
+    }
+  }
+  if (flag == false) return false;
+
+  if (Number(sampleActivity) > MODERN_ACTIVITY || Number(sampleActivity) < 0)
+    return false;
 
   return Math.round(Math.log(MODERN_ACTIVITY / Number(sampleActivity)) / k);
 }
